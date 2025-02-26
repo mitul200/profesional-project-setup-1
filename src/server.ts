b5127ffe06bfa9f0f67ab2fdecd5dafd0f34e9a0
mitul200 <mitul5267@gmail.com>
2025-02-26 23:01:06 +0600

    professional project setup backed expressjsnodejs & mongodb

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import app from "./app";
+import config from "./app/config";
+import mongoose from "mongoose";
+
+// pass authentication = admin2003
+// DATABASE_URL=mongodb+srv://mitul5267:lIQajUg7BpewMtVj@cluster0.bsnes.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+// "mongodb://127.0.0.1:27017/test"
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+    
+  } catch (error) {
+    console.log(error);
+  }
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
