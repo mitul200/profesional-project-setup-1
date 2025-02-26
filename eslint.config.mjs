460eebf02d5fc0a5448e29542a108021471e8713
mitul200 <mitul5267@gmail.com>
2025-02-26 23:55:40 +0600

    setup lint

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { languageOptions: { globals: globals.browser } },
+  {
+    ignores: [".node_modules/*", "dist/*"],
+  },
+  {
+    rules: {
+      eqeqeq: "off",
+      "no-unused-vars": "error",
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+    },
+  },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
--- src/server.ts
+++ src/server.ts
@@ -13,10 +13,11 @@ async function main() {
     app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
-    
   } catch (error) {
     console.log(error);
   }
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
+
+main();
